43c22b03d9fd5d81a0b2f42fe70f5b8c414b63b6
Kamesh-369 <74868762+Kamesh-369@users.noreply.github.com>
2022-04-29 11:34:01 +0530

    CoureseRegistration-MEAN

---
+++ Routes/authRoutes.js
@@ -0,0 +1,80 @@
+const exp = require('express')
+
+const routes = exp.Router();
+
+const User=require("../models/User");
+
+const {hashGenerate}=require("../helpers/hashing");
+const {hashValidator}=require("../helpers/hashing");
+
+routes.post("/register",async (req,res)=>{
+  try {
+
+    const hashPassword= await hashGenerate(req.body.password);
+    const userEmail = await req.body.email;
+    const dbEmail = await User.findOne({userEmail});
+    if(dbEmail)
+    {
+      return res.status(400).send("User Exist");
+    }
+    const user = new User({
+      
+        username:req.body.username,
+        email:req.body.email,
+        phoneno:req.body.phoneno,
+        password:hashPassword
+        
+    });
+
+    const savedUser = await user.save()
+    res.send(savedUser);
+    
+  } catch (error) {
+    res.send(error);
+  }
+
+    
+})
+
+routes.post("/login",async (req,res)=>{
+  try {
+    const existingUser = await User.findOne({email:req.body.email})
+    
+    if(!existingUser)
+    {
+      res.send("Email Id Mismatch");
+    }
+    else
+    {
+      const checkUser = await hashValidator(req.body.password,existingUser.password)
+      if(!checkUser)
+      {
+        res.send("Password is Invalid");
+        return;
+      }
+      res.send("Login Successful.....");
+    }
+
+
+  } catch (error) {
+    res.send(error);
+    
+  }
+    
+
+    
+})
+
+
+routes.get("/users", async (request, response) => {
+    const user = await User.find({});
+  
+    try {
+      response.send(user);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  });
+
+
+module.exports = routes;
---
+++ index.js
@@ -0,0 +1,44 @@
+const exp = require('express')
+
+const mongoose=require("mongoose")
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+mongoose.connect(process.env.DB_URL,()=>{console.log("DB Connected")})
+
+const app = exp()
+
+const authRoutes=require("./routes/authRoutes")
+
+app.use(exp.json());
+app.use("/api/register",authRoutes);
+
+
+
+
+
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Home Page')
+// })
+
+// app.get('/users',auth, (req, res) => {
+//   res.send('Users Page')
+// })
+
+
+// function auth(req, res, next) {
+//     if (req.query.admin === 'true') {
+//       next()
+//     } else {
+//       res.send('ERROR: You must be an admin')
+//     }
+//   }
+
+
+
+app.listen(3000, () => console.log('Server Started'))
